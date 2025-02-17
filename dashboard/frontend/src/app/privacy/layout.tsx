ef753b6201c60201701ac556a98eb39f60ec00a8
charles javelona <charlesjavelona@gmail.com>
2025-02-17 17:37:07 -0500

    Added layout

---
+++ dashboard/frontend/src/app/privacy/layout.tsx
@@ -0,0 +1,36 @@
+import "../globals.css";
+import { JetBrains_Mono } from 'next/font/google';
+import { Plus_Jakarta_Sans } from 'next/font/google';
+
+const jakarta = Plus_Jakarta_Sans({
+  subsets: ['latin'],
+  variable: '--font-primary',
+  display: 'swap',
+});
+
+const jetbrainsMono = JetBrains_Mono({
+  subsets: ['latin'],
+  variable: '--font-mono',
+  display: 'swap',
+});
+
+export const metadata = {
+  title: "Privacy Policy & Terms of Service",
+  description: "See our privacy policy and terms of service",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" className={`${jakarta.variable} ${jetbrainsMono.variable}`}>
+        <body
+          className={`${jakarta.variable} ${jetbrainsMono.variable} antialiased`}
+        >
+          {children}
+        </body>
+    </html>
+  );
+}
